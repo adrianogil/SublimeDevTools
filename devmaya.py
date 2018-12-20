e30ec190f4e60b9bb321c58c647ec1aa28577269
Adriano Gil <adrianogil.san@gmail.com>
2018-12-20 08:13:13 -0400

    Generate call for Maya scripts

--- devmaya.py
+++ devmaya.py
@@ -1,5 +1,4 @@
 import sublime, sublime_plugin, subprocess, os
-
 import webbrowser
 
 
@@ -13,3 +12,20 @@ class ShowMayaDocs(sublime_plugin.TextCommand):
                             ".html"
 
             webbrowser.open(maya_docs_url, autoraise=True)
+
+
+class GenerateMayaCall(sublime_plugin.TextCommand):
+    def run(self, edit):
+        current_file = self.view.file_name()
+
+        script_path = os.path.dirname(current_file)
+        module_name = os.path.basename(current_file)
+        module_name = module_name.replace('.py', '')
+
+        call_str = 'import sys\n'
+        call_str += 'script_path = "' + script_path + '"\n'
+        call_str += 'sys.path.insert(0, script_path)\n\n'
+        call_str += 'import ' + module_name + '\n\n'
+        call_str += 'reload(' + module_name + ')\n'
+
+        sublime.set_clipboard(call_str)
