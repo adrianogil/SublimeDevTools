6c790959695bbd99d313319d2d437e2312d04d52
Adriano Gil <adrianogil.san@gmail.com>
2018-12-28 19:00:29 -0400

    Improved call maya command

--- devmaya.py
+++ devmaya.py
@@ -25,7 +25,9 @@ class GenerateMayaCall(sublime_plugin.TextCommand):
         call_str = 'import sys\n'
         call_str += 'script_path = "' + script_path + '"\n'
         call_str += 'sys.path.insert(0, script_path)\n\n'
+        call_str += 'loaded = "' + module_name + '" in sys.modules\n\n'
         call_str += 'import ' + module_name + '\n\n'
-        call_str += 'reload(' + module_name + ')\n'
+        call_str += 'if loaded:\n'
+        call_str += '\treload(' + module_name + ')\n'
 
         sublime.set_clipboard(call_str)
